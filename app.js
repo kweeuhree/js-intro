37c4235bcf3790442741585384977f2f427e942f
Nika K <firstnamenika@gmail.com>
2024-03-13 18:33:11 -0400

    updated up to math

--- app.js
+++ app.js
@@ -4,16 +4,29 @@ const n2 = 15;
 const n3 = 20;
 const n4 = 5;
 
-const isDivisibleby5 = ((n1%5) + (n2%5) + (n3%5) + (n4%5));
+//check if the first number is larger than the last
+const firstLargerThanLast = n1> n4;
+console.log(`Is first number larger than the last? ${firstLargerThanLast}`);
 
-console.log(isDivisibleby5===0);
+// subtract the first number from the second number
+let arithmeticChain = n2 - n1;
+// Multiply the result by the third number
+arithmeticChain *= n3;
+//Find the remainder of dividing the result by the fourth number
+arithmeticChain %= n4;
+
+console.log(`Result of the arithmetic chain: ${arithmeticChain}`);
+
+const isDivisibleby5 = ((n1%5) + (n2%5) + (n3%5) + (n4%5)) === 0;
+
+console.log(`Are all numbers divisible by 5? ${isDivisibleby5}`);
 
 
 // Check one: add up to 50
 // This is a fairly simple operation using
 // arithmetic operators and a comparison.
 const isSum50 = (n1 + n2 + n3 + n4) == 50;
-const result1 = `Does isSum50 add up to 50: ${isSum50}`;
+const result1 = `Do all numbers add up to 50: ${isSum50}`;
 console.log(result1);
 
 // Check two: at least two odd numbers
@@ -21,19 +34,32 @@ console.log(result1);
 // Since % 2 is 0 if even and 1 if odd, we can use
 // arithmetic to count the total number of odd numbers.
 const isTwoOdd = (n1 % 2) + (n2 % 2) + (n3 % 2) + (n4 % 2) >= 2;
-const result2 = `Is isTwoOdd odd: ${isTwoOdd}`;
+const result2 = `Are all numbers odd: ${isTwoOdd}`;
 console.log(result2);
 
 // Check three: no number larger than 25
 // This time, we use the OR operator to check
 // if ANY of the numbers is larger than 25.
 const isOver25 = n1 > 25 || n2 > 25 || n3 > 25 || n4 > 25;
+let result3 = `Is any of the numbers larger than 25: ${isOver25}`;
+
+console.log(result3);
+
+// Modify isOver25
+
+const isNotOver25 = n1 > 25 && n2 > 25 && n3 > 25 && n4 > 25;
+let result31 = `Is any of the numbers larger than 25: ${isNotOver25}`;
+
+console.log(result31);
 
 // Check four: all unique numbers
 // This is long, and there are more efficient
 // ways of handling it with other data structures
 // that we will review later.
 const isUnique = n1 != n2 && n1 != n3 && n1 != n4 && n2 != n3 && n2 != n4 && n3 != n4;
+let result4 = `Are all numbers unique? ${isUnique}`;
+
+console.log(result4);
 
 // Here, we put the results into a single variable 
 // for convenience. Note how we negate isOver25 using
@@ -52,3 +78,6 @@ const dontDoThis = ((n1 + n2 + n3 + n4) == 50) &&
   ((n1 % 2) + (n2 % 2) + (n3 % 2) + (n4 % 2) >= 2) && 
   !(n1 > 25 || n2 > 25 || n3 > 25 || n4 > 25) && 
   (n1 != n2 && n1 != n3 && n1 != n4 && n2 != n3 && n2 != n4 && n3 != n4);
+
+
+//   Math Problems
